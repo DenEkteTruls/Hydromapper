faa05be2c1ecca48af3fe13ffbcdd2ed1d30985b
= <=>
2021-11-16 21:26:30 +0100

    fixed python3.10 bug

--- nav/nav.py
+++ nav/nav.py
@@ -66,6 +66,7 @@ class Nav:
 
             if msg[0:2] == "0x1":
                 for esc in self.escs:
+                    print(msg[3:])
                     esc.set(msg[3:])
 
             elif msg[0:2] == "2x":
