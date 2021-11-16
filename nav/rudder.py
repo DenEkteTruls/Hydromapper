45915d3f789beaf5853e94633870a46f83d6d4ff
= <=>
2021-11-16 21:04:11 +0100

    fixed python3.10 bug

--- nav/rudder.py
+++ nav/rudder.py
@@ -10,10 +10,10 @@ class Rudder:
         self.mid = 1050
         self.left = 1500
 
-        self.degrees = np.linspace(self.right, self.left, 120)
+        self.degrees = np.linspace(self.right-2, self.left+2, 120)
 
 
     def set_heading(self, heading : int) -> None:
 
-        hz = self.degrees[heading+60]
+        hz = self.degrees[heading+59]
         os.system(f"pigs s {self.pin} {hz}")
