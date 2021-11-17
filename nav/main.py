b09b47cff69b8297f449672c5d1b4bd05220571f
DenEkteTruls <casper_nag@outlook.com>
2021-11-17 10:32:56 +0100

    bug fixed

--- nav/main.py
+++ nav/main.py
@@ -7,8 +7,8 @@ import time
 
 pi = pigpio.pi()
 nav = Nav()
-esc = ESC(esc_pin = 4)
-rudder = Rudder(pin = 17)
+esc = ESC(esc_pin = 4, pi = pi)
+rudder = Rudder(pin = 17, pi = pi)
 net = Networking("localhost", 8081, 1024)
 
 nav.add_esc(esc)
