ba9aff6742fc70d19030941258e4a026b0f3c89a
DenEkteTruls <casper_nag@outlook.com>
2021-12-08 12:21:09 +0100

    fix sonarReader

--- sonarReader.py
+++ sonarReader.py
@@ -5,6 +5,8 @@ import pytesseract as pt
 
 cap = cv2.VideoCapture(0)
 pt.pytesseract.tesseract_cmd = "C:/Program files/Tesseract-OCR/tesseract.exe"
+depthBuffer = []
+depth = None
 
 while True:
 
@@ -12,21 +14,26 @@ while True:
 
     HSV = cv2.resize(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), (480, 360))
 
-    sensitivity = 50
+    sensitivity = 60
     lower_white = np.array([0, 0, 255-sensitivity])
     upper_white = np.array([255, sensitivity, 255])
 
     mask = cv2.inRange(HSV, lower_white, upper_white)
     HSV = cv2.bitwise_and(HSV, HSV, mask = mask)
+    HSV = cv2.GaussianBlur(HSV, (3, 3), 0)
+
 
     d = pt.image_to_data(HSV, config = "--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789.", output_type = pt.Output.DICT)
-    n_boxes = len(d['level'])
-    for i in range(n_boxes):
-        (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
-        cv2.rectangle(HSV, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    print(d['text'])
+    # filter
+    try    : depthBuffer.append(float(d['text'][4]))
+    except : pass
+
+    if len(depthBuffer) > 1:
+        depth = max(set(depthBuffer), key = depthBuffer.count)
+        depthBuffer = []
+
+    print(depth)
 
     cv2.imshow("frame", frame)
     cv2.imshow("masked", HSV)
