df38499a30d53e4e3ed691506a9bec62f177fd46
= <=>
2022-01-22 00:57:57 +0100

    10.8

--- nav/main.py
+++ nav/main.py
@@ -29,6 +29,8 @@ while nav.running or nav.retHome:
     #print(f"HEADING: {nav.GPScompass}\tOFFSET: {nav.offset}\tSPEED: {nav.depth}\tPOSITION: {nav.position}")
     #print(nav.GPS)
 
+    print(nav.position)
+
     if not nav.handle_networking(net.recieved):
         nav.running = False
         net.running = False
