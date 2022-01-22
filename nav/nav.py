091205a9e37acb5afec272e5674a0741f64c802d
= <=>
2022-01-22 13:01:10 +0100

    10.9

--- nav/nav.py
+++ nav/nav.py
@@ -255,6 +255,7 @@ class Nav:
                     rudder.heading_compansation(self.offset)
 
                 if print_:
+                    pass
                     #self.save_data(self.position, self.depth, self.heading, self.depthshot)
                     #print(f"[AUTOPILOT] {i}\tSatellites: {self.sats}\tHeading: {self.heading}\tOffset: {self.offset}\tDistance: {distance}m\tPosition: {self.position}"); print_ = False
 
