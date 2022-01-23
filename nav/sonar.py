d349c652ef1c51e521980fc578076cbd0ea09785
= <=>
2022-01-23 15:15:53 +0100

    13.0

--- nav/sonar.py
+++ nav/sonar.py
@@ -18,7 +18,7 @@ class Sonar:
         while self.running:
 
             frame = self.cap.read()[1]
-            gray = cv2.resize(frame, (480, 360))#cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (480, 360))
+            gray = cv2.resize(frame, (640, 480))#cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), (480, 360))
             d = pt.image_to_data(gray, config = "--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789.", output_type = pt.Output.DICT)
 
             try     : self.depth = float(d['text'][4])
